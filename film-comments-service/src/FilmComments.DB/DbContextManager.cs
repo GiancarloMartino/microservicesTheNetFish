94938402052a5cb00d51504972146ef2f3eac734
Giancarlo <gianmart83@gmail.com>
2021-11-12 23:20:08 +0100

    Service DotNet per la gestione commenti di TheNetFish

---
+++ film-comments-service/src/FilmComments.DB/DbContextManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FilmComments.DB.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmComments.DB
+{
+    public class DbContextManager : DbContext
+    {
+        public DbSet<CommentEntity> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=thenetfish;Uid=root;Pwd=root";
+            var version = new MySqlServerVersion(new Version(5, 7, 34));
+
+            optionsBuilder.UseMySql(connectionString, version);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
